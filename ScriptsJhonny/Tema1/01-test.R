6f1f320a4bae4a7520ecbb4d29ec1ddd2555ed5e
jrosero8 <jrosero8@gmail.com>
2020-05-03 19:05:23 -0500

    Commit from RStudio

---
+++ ScriptsJhonny/Tema1/01-test.R
@@ -0,0 +1,6 @@
+#Instalar Latex, antes descargar e instalar miktex
+library(tinytex)
+tinytex::install_tinytex()
+tinytex:::is_tinytex()
+
+
---
+++ ScriptsJhonny/Tema1/03-installpackages.R
@@ -0,0 +1,4 @@
+install.packages("tidyverse", dep = TRUE)
+library(tidyverse)
+install.packages("magic", dep = TRUE)
+library(magic)
